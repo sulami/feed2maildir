e7bf355297ad185a462cf6491900bc50b53aadf0
Robin Schroer <sulami@peerwire.org>
2014-11-25 12:21:17 +0100

    store config in json format

--- feed2maildir/loader.py
+++ feed2maildir/loader.py
@@ -1,7 +1,9 @@
+import json
+
 class Loader:
     """Loads the config/list of feeds from a file"""
 
     def __init__(self, config='~/.f2mrc'):
         with file(config, 'r') as f:
-            self.config = f.read()
+            self.config = json.loads(f.read())
 
--- feed2maildir/tests/loadertests.py
+++ feed2maildir/tests/loadertests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import json
 import unittest
 
 import feed2maildir.loader
@@ -8,10 +9,12 @@ import feed2maildir.loader
 class LoaderTestCase(unittest.TestCase):
     def test_loader_reads_file(self):
         testfile = '/tmp/f2mrc'
+        testfeeds = {'feeds': {'Feed #1': 'someurl', 'Feed #2': 'anotherurl'}}
         with open(testfile, 'w') as f:
-            f.write('toast')
+            f.write(json.dumps(testfeeds))
         loader = feed2maildir.loader.Loader(config=testfile)
-        self.assertEqual(loader.config, 'toast')
+        self.assertEqual(loader.config['feeds']['Feed #1'], 'someurl')
+        self.assertEqual(loader.config['feeds']['Feed #2'], 'anotherurl')
 
 if __name__ == '__main__':
     unittest.main()

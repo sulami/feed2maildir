8e5df7551a0b37b2bb4d137511603db2d927247b
Robin Schroer <sulami@peerwire.org>
2014-11-28 10:38:32 +0100

    simplify runner and add basic cli-argmuments

--- feed2maildir/main.py
+++ feed2maildir/main.py
@@ -1,15 +1,3 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from feed2maildir.converter import Converter
-from feed2maildir.loader import Loader
-from feed2maildir.reader import Reader
-
-def main():
-    loader = Loader()
-    config = loader.config
-    reader = Reader(config['feeds'])
-    converter = Converter(db=config['db'], maildir=config['maildir'])
-    converter.load(reader.feeds)
-    converter.run()
-
